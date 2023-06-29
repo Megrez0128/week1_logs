60385af5cf4dc53b4c1090f16349eb526e4e3e5f
Megrez0128 <dnimpeed@163.com>
2023-06-29 16:47:25 +0800

    更新脚本

--- select_currency.sh
+++ select_currency.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-grep -E "^Currency|" currency_flow.txt > test.txt
+grep "INFO - Currency|" 111.1 > test.txt
+grep -E '^.{25}INFO - Currency' 111.1 > test.txt
 # 读取currency_flow.txt文件，将筛选出的行存入test.txt文件
