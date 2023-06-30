4c9c348b5c0c1a7c53e50af50d027081e483224d
张程皓 <76559335+zhangchenghao011114@users.noreply.github.com>
2023-06-30 11:50:24 +0800

    Add temporary var

--- src/main/java/org/example/LogAnalyzer.java
+++ src/main/java/org/example/LogAnalyzer.java
@@ -13,6 +13,8 @@ public class LogAnalyzer {
     public Map<Integer,Map<String, Map<String,Map<String,String>>>> summarizeByServerUserChar(List<CurrencyLog> currencyLogs) {
         //统计出区服->用户->角色->的totalConsumptionValue和totalAddedValue信息
         serverUserCharSummary = new HashMap<>();
+        Map<String, Map<String, Map<String, String>>> stringMapMap;
+        Map<String, Map<String, String>> stringMapMap1;
         for (CurrencyLog currencyLog : currencyLogs) {
             int iZoneAreaID = currencyLog.getiZoneAreaID();
             //对于每一条log，如果其区服信息不在serverUserCharSummary中，则新建一个区服信息
@@ -21,20 +23,22 @@ public class LogAnalyzer {
             }
             String userID = currencyLog.getvUserID();
             //对于每一条log，如果其用户信息不在serverUserCharSummary中，则新建一个用户信息
-            if (!serverUserCharSummary.get(iZoneAreaID).containsKey(userID)) {
-                serverUserCharSummary.get(iZoneAreaID).put(userID,new HashMap<>());
+            stringMapMap = serverUserCharSummary.get(iZoneAreaID);
+            if (!stringMapMap.containsKey(userID)) {
+                stringMapMap.put(userID,new HashMap<>());
             }
             String roleID = currencyLog.getvRoleID();
             //对于每一条log，如果其角色信息不在serverUserCharSummary中，则新建一个角色信息
-            if (!serverUserCharSummary.get(iZoneAreaID).get(userID).containsKey(roleID)) {
+            stringMapMap1 = stringMapMap.get(userID);
+            if (!stringMapMap1.containsKey(roleID)) {
                 //新建一个map，key为"totalAddedValue"和"totalConsumptionValue"，value为0
                 Map<String,String> map = new HashMap<>();
                 map.put("totalAddedValue","0");
                 map.put("totalConsumptionValue","0");
-                serverUserCharSummary.get(iZoneAreaID).get(userID).put(roleID, map);
+                stringMapMap1.put(roleID, map);
             }
             //如果角色信息已经存在，则更新其消耗额度和增加额度
-            Map<String,String> map = serverUserCharSummary.get(iZoneAreaID).get(userID).get(roleID);
+            Map<String,String> map = stringMapMap1.get(roleID);
             if(currencyLog.getChangeType() == 1){ //增加
                 map.put("totalAddedValue",String.valueOf(Integer.parseInt(map.get("totalAddedValue")) + currencyLog.getChangeValue()));
             }else if(currencyLog.getChangeType() == 2){ //消耗
@@ -50,26 +54,32 @@ public class LogAnalyzer {
     public Map<Integer,Map<String, Map<String,Map<Integer,Map<String,String>>>>> summarizeByServerUserCharReason(List<CurrencyLog> currencyLogs) {
         //统计出区服->用户->角色->LogReason的totalConsumptionValue和totalAddedValue信息
         serverUserCharReasonSummary = new HashMap<>();
+        Map<String, Map<String, Map<Integer, Map<String, String>>>> stringMapMap;
+        Map<String, Map<Integer, Map<String, String>>> stringMapMap1;
+        Map<Integer, Map<String, String>> integerMapMap;
         for(CurrencyLog currencyLog : currencyLogs) {
             int iZoneAreaID = currencyLog.getiZoneAreaID();
             if(!serverUserCharReasonSummary.containsKey(iZoneAreaID)) {
                 serverUserCharReasonSummary.put(iZoneAreaID,new HashMap<>());
             }
             String userID = currencyLog.getvUserID();
-            if(!serverUserCharReasonSummary.get(iZoneAreaID).containsKey(userID)) {
-                serverUserCharReasonSummary.get(iZoneAreaID).put(userID,new HashMap<>());
+            stringMapMap = serverUserCharReasonSummary.get(iZoneAreaID);
+            if(!stringMapMap.containsKey(userID)) {
+                stringMapMap.put(userID,new HashMap<>());
             }
             String roleID = currencyLog.getvRoleID();
-            if(!serverUserCharReasonSummary.get(iZoneAreaID).get(userID).containsKey(roleID)) {
-                serverUserCharReasonSummary.get(iZoneAreaID).get(userID).put(roleID,new HashMap<>());
+            stringMapMap1 = stringMapMap.get(userID);
+            if(!stringMapMap1.containsKey(roleID)) {
+                stringMapMap1.put(roleID,new HashMap<>());
             }
-            if(!serverUserCharReasonSummary.get(iZoneAreaID).get(userID).get(roleID).containsKey(currencyLog.getMainReason())) {
+            integerMapMap = stringMapMap1.get(roleID);
+            if(!integerMapMap.containsKey(currencyLog.getMainReason())) {
                 Map<String,String> map = new HashMap<>();
                 map.put("totalAddedValue","0");
                 map.put("totalConsumptionValue","0");
-                serverUserCharReasonSummary.get(iZoneAreaID).get(userID).get(roleID).put(currencyLog.getMainReason(),map);
+                integerMapMap.put(currencyLog.getMainReason(),map);
             }
-            Map<String,String> map = serverUserCharReasonSummary.get(iZoneAreaID).get(userID).get(roleID).get(currencyLog.getMainReason());
+            Map<String,String> map = integerMapMap.get(currencyLog.getMainReason());
             if(currencyLog.getChangeType() == 1) {
                 map.put("totalAddedValue",String.valueOf(Integer.parseInt(map.get("totalAddedValue")) + currencyLog.getChangeValue()));
             }else if(currencyLog.getChangeType() == 2) {
@@ -80,20 +90,26 @@ public class LogAnalyzer {
     }
 
     public String query(Integer tmpSvrId, String vUserID, String vRoleID, Integer mainReason) {
-
+        Map<String, Map<String, Map<Integer, Map<String, String>>>> stringMapMap;
+        Map<String, Map<Integer, Map<String, String>>> stringMapMap1;
+        Map<Integer, Map<String, String>> integerMapMap;
         if(!serverUserCharReasonSummary.containsKey(tmpSvrId)) {
             return "Server " + tmpSvrId + " doesn't exist!";
         }
-        if(!serverUserCharReasonSummary.get(tmpSvrId).containsKey(vUserID)) {
+
+        stringMapMap = serverUserCharReasonSummary.get(tmpSvrId);
+        if(!stringMapMap.containsKey(vUserID)) {
             return "User " + vUserID + " doesn't exist in Server " + tmpSvrId;
         }
-        if(!serverUserCharReasonSummary.get(tmpSvrId).get(vUserID).containsKey(vRoleID)) {
+        stringMapMap1 = stringMapMap.get(vUserID);
+        if(!stringMapMap1.containsKey(vRoleID)) {
             return "Role " + vRoleID + " doesn't exist under UserID "  + vUserID + " in Server " + tmpSvrId;
         }
-        if(!serverUserCharReasonSummary.get(tmpSvrId).get(vUserID).get(vRoleID).containsKey(mainReason)) {
+        integerMapMap = stringMapMap1.get(vRoleID);
+        if(!integerMapMap.containsKey(mainReason)) {
             return "Reason " + mainReason + " doesn't exist under RoleID " + vRoleID + " under UserID "  + vUserID + " in Server " + tmpSvrId;
         }
-        Map<String,String> map = serverUserCharReasonSummary.get(tmpSvrId).get(vUserID).get(vRoleID).get(mainReason);
+        Map<String,String> map = integerMapMap.get(mainReason);
         return "totalAddedValue:" + map.get("totalAddedValue") + "  totalConsumptionValue:" + map.get("totalConsumptionValue");
     }
 }
